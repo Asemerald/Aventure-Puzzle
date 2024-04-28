90a2a201f61a95508f5494c799d8eeaac3708a75
Tonin <117942487+Toninga@users.noreply.github.com>
2024-04-28 21:26:28 +0200

    Big update
    Création de tout pleins de préfabs de décor, import de pleins de textures et de low polys, rangement de dossiers, correction de shaders de fog, etc.
    Ajout de bougies, lanterne, texturing de fruits, de bois, vases, etc.

---
+++ Assets/_dev/Tonin/Scripts/LightFlickering.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LightFlickering : MonoBehaviour
+{
+
+    private float _defaultIntensity;
+
+    public Vector2 noiseRange = new Vector2(0,0);
+    private Vector2 noiseSample = new Vector2(0, 0);
+    public float noiseSpeed;
+
+    private Light lightRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.GetComponent<Light>() != null)
+        {
+            lightRef = gameObject.GetComponent<Light>();
+            _defaultIntensity = lightRef.intensity;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        lightRef.intensity = _defaultIntensity + Map(Mathf.PerlinNoise(noiseSample.x, noiseSample.y), 0, 1, noiseRange.x, noiseRange.y);
+        noiseSample.x += noiseSpeed * Time.deltaTime;
+    }
+    
+    float Map(float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s-a1)*(b2-b1)/(a2-a1);
+    }
+}
