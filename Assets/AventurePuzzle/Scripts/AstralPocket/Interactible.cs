411d6566d31517a01168a776c1de4bd619cf5c1e
Wardyom <c.huyghe@rubika-edu.com>
2024-05-06 22:16:08 +0200

    interactibleBase

--- Assets/AventurePuzzle/Scripts/AstralPocket/Interactible.cs
+++ Assets/AventurePuzzle/Scripts/AstralPocket/Interactible.cs
@@ -4,15 +4,105 @@ using UnityEngine;
 
 public class Interactible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /* None -> Aucun état
+     * Moveable -> l'objet à une collision et peut être déplacer
+     * UnMoveable -> l'objet à une collision et ne peut pas être déplacer
+     * NoCollider -> l'objet n'a pas de collision et ne peut pas être déplacer
+     * EmitEnergy -> l'objet est dans l'état Moveable et émet de l'energie dans un rayon autour de lui
+     * EnergyUnMoveable -> l'objet émet de l'energie dans un rayon autour de lui, à une collision et ne peut pas être déplacer
+     * EnergyNoCollider -> l'objet émet de l'energie dans un rayon autour de lui, n'a pas de collision et ne peut pas être déplacer
+     * Size -> l'objet change de taille/de forme et à une collision
+     * EnergySize -> l'objet change de taille/de forme et à une collision et émet de l'energie dans un rayon autour de lui
+     * Portal -> l'objet se transforme en portail
+     */
+
+    public enum ObjectState { None, Moveable, UnMoveable, NoCollider, EmitEnergy, EnergyUnMoveable, EnergyNoCollider, Size, EnergySize, Portal }
+
+    public ObjectState worldState;
+    public ObjectState astralState;
+
+    public bool isMoveable;
+    public bool emitEnergy;
+
+    public void SwitchMode(bool astral)
     {
-        
+        if(astral)
+        {
+            switch (astralState)
+            {
+                case ObjectState.None:
+                    break;
+                case ObjectState.Moveable:
+                    break;
+                case ObjectState.UnMoveable:
+                    break;
+                case ObjectState.NoCollider:
+                    break;
+                case ObjectState.EmitEnergy:
+                    break;
+                case ObjectState.EnergyUnMoveable:
+                    break;
+                case ObjectState.EnergyNoCollider:
+                    break;
+                case ObjectState.Size:
+                    break;
+                case ObjectState.EnergySize:
+                    break;
+                case ObjectState.Portal:
+                    break;
+
+            }
+        }
+        else
+        { switch (worldState)
+            {
+                case ObjectState.None:
+
+                    break;
+                case ObjectState.Moveable:
+
+                    break;
+                case ObjectState.UnMoveable:
+
+                    break;
+                case ObjectState.NoCollider:
+
+                    break;
+                case ObjectState.EmitEnergy:
+
+                    break;
+                case ObjectState.EnergyUnMoveable:
+
+                    break;
+                case ObjectState.EnergyNoCollider:
+
+                    break;
+                case ObjectState.Size:
+
+                    break;
+                case ObjectState.EnergySize:
+
+                    break;
+                case ObjectState.Portal:
+
+                    break;
+            } 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    Collider objectCol;
+
+    private void Start()
     {
-        
+        objectCol = GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        if (emitEnergy)
+        {
+
+        }
     }
 }
