1a30d9427bb77864db8ccfd538b4cee1e043c1f7
Asemerald <63583036+Asemerald@users.noreply.github.com>
2024-05-18 18:36:27 +0200

    add anim

--- Assets/AventurePuzzle/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/AventurePuzzle/Scripts/PlayerScripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
 
     Vector2 moveInputs;
     private Vector3 move;
+    
+    private PlayerController Instance;
+    private PlayerAnimator _playerAnimator;
+    
 
     [Header("Move Settings")]
     [SerializeField] private float maxSpeed = 8f;
@@ -43,11 +47,33 @@ public class PlayerController : MonoBehaviour
     private int currentHealth;*/
 
     GameObject currentGrabObject;
-    [HideInInspector] public float MoveSpeed;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance= this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        if (TryGetComponent(out PlayerAnimator playerAnimator))
+        {
+            _playerAnimator = playerAnimator;
+        }
+        else Debug.LogError("No PlayerAnimator component found on " + gameObject.name);
+        
+        if (TryGetComponent(out Rigidbody Rigidbody))
+        {
+            rb = Rigidbody;
+        }
+        else Debug.LogError("No Rigidbody component found on " + gameObject.name);
+        
         rb.freezeRotation = true;
 
         //currentHealth = maxHealth;
@@ -146,6 +172,13 @@ public class PlayerController : MonoBehaviour
             force = new Vector3(movement.x * acceleration, rb.velocity.y, movement.z * acceleration);
 
         rb.AddForce(force, ForceMode.Acceleration);
+        
+
+        // Calcul la speed et l'envoie a l'animator
+        float speed = rb.velocity.magnitude / maxSpeed;
+        speed = Mathf.Clamp(speed, 0f, 1f);
+        _playerAnimator.SetSpeed(speed);
+        
     }
 
     private void CameraOffset()
