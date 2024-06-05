8a85264bca86429261ff3b49702705285266f185
Asemerald <63583036+Asemerald@users.noreply.github.com>
2024-06-05 17:38:35 +0200

    uwu

--- Assets/ConfinerSwitcher.cs
+++ Assets/ConfinerSwitcher.cs
@@ -6,16 +6,40 @@ using UnityEngine;
 
 public class ConfinerSwitcher : MonoBehaviour
 {
-
-    private GameObject _CameraFollowPlayer;
+    
+    private CinemachineConfiner _cinemachineConfiner;
     
     private void Start()
     {
-        _CameraFollowPlayer = GameObject.FindGameObjectWithTag("CameraFollow");
+        _cinemachineConfiner = FindObjectOfType<CinemachineConfiner>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
-        _CameraFollowPlayer.GetComponent<CinemachineConfiner>().m_BoundingVolume = transform.parent.GetComponent<Collider>();
+        //TRY GET collider in parent, if no collider found get mesh collider in parent
+       
+        if (transform.parent.TryGetComponent(out Collider parentCollider))
+        {
+            _cinemachineConfiner.m_Damping = 2;
+            _cinemachineConfiner.m_BoundingVolume = parentCollider;
+            
+            //Start a coroutine to reset damping after 2s
+            StartCoroutine(ResetDamping());
+        }
+        else if (transform.parent.TryGetComponent(out MeshCollider meshCollider))
+        {
+            _cinemachineConfiner.m_Damping = 2f;
+            _cinemachineConfiner.m_BoundingVolume = meshCollider;
+            
+            //Start a coroutine to reset damping after 2s
+            StartCoroutine(ResetDamping());
+        }
     }
+    
+    private IEnumerator ResetDamping()
+    {
+        yield return new WaitForSeconds(2f);
+        _cinemachineConfiner.m_Damping = 0f;
+    }
+    
 }
