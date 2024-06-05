dd712ab6567888e4da4a3390a00df1336b765bb6
Asemerald <63583036+Asemerald@users.noreply.github.com>
2024-06-05 20:53:58 +0200

    uwu

--- Assets/ConfinerSwitcher.cs
+++ Assets/ConfinerSwitcher.cs
@@ -2,20 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using Cinemachine.PostFX;
 using UnityEngine;
 
 public class ConfinerSwitcher : MonoBehaviour
 {
     
     private CinemachineConfiner _cinemachineConfiner;
+    private CinemachineVirtualCamera _cameraTrackedDolly;
     
+    private CinemachineVirtualCamera _cinemachineVirtualCamera;
+
+    [SerializeField] private bool SwitchToDollyTrack;
+     
     private void Start()
     {
         _cinemachineConfiner = FindObjectOfType<CinemachineConfiner>();
+        _cinemachineVirtualCamera = _cinemachineConfiner.GetComponent<CinemachineVirtualCamera>();
+        _cameraTrackedDolly = FindObjectOfType<CinemachinePostProcessing>().gameObject.GetComponent<CinemachineVirtualCamera>();
     }
 
     private void OnTriggerExit(Collider other)
     {
+        
+        if (_cinemachineVirtualCamera.Priority != 10)
+        {
+            _cinemachineVirtualCamera.Priority = 10;   
+        }
+        
+        
         //TRY GET collider in parent, if no collider found get mesh collider in parent
        
         if (transform.parent.TryGetComponent(out Collider parentCollider))
@@ -35,7 +50,17 @@ public class ConfinerSwitcher : MonoBehaviour
             StartCoroutine(ResetDamping());
         }
     }
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (SwitchToDollyTrack)
+        {
+            _cinemachineVirtualCamera.Priority = 0;
+            //switch _cinemachineConfiner.GetComponent<CinemachineVirtualCamera>() to dolly track
+            _cameraTrackedDolly.GetComponent<CinemachineTrackedDolly>().m_Path = transform.parent.GetComponent<CinemachinePath>();
+        }
+    }
+
     private IEnumerator ResetDamping()
     {
         yield return new WaitForSeconds(2f);
