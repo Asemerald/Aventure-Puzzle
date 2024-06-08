191826cedd6c532535af71be719dc9bad69901c7
Asemerald <63583036+Asemerald@users.noreply.github.com>
2024-06-08 21:00:25 +0200

    1

--- Assets/MainMenuButton.cs
+++ Assets/MainMenuButton.cs
@@ -14,7 +14,12 @@ public class MainMenuButton : MonoBehaviour
     
     private EventSystem eventSystem;
 
+    
+    public bool isOptionsButton;
+    public bool isChapterButton;
     public bool isPlayButton;
+    public bool isCreditsButton;
+    public bool isQuitButton;
     
     private void Start()
     {
@@ -22,11 +27,36 @@ public class MainMenuButton : MonoBehaviour
         eventSystem = EventSystem.current;
         
     }
-
-    private void Update()
+    
+    public void IsHovering(bool isHovering)
     {
-        card.SetBool("Hover", eventSystem.currentSelectedGameObject == gameObject);
+        card.SetBool("Hover", isHovering);
+    }
+    
+    public void IsClicked()
+    {
+        card.SetTrigger("Clicked");
         
+        if (isOptionsButton)
+        {
+           MainMenuManager.Instance.OpenMenuPanel(1);
+        }
+        else if (isChapterButton)
+        {
+            MainMenuManager.Instance.OpenMenuPanel(2);
+        }
+        else if (isPlayButton)
+        {
+            MainMenuManager.Instance.OpenMenuPanel(3);
+        }
+        else if (isCreditsButton)
+        {
+            MainMenuManager.Instance.OpenMenuPanel(4);
+        }
+        else if (isQuitButton)
+        {
+            MainMenuManager.Instance.OpenMenuPanel(5);
+        }
     }
     
     
--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -8,6 +8,8 @@ using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+   
+   public static MainMenuManager Instance { get; private set; }
    [SerializeField] private Button PlayButton;
    [SerializeField] private Material DeskMaterial;
    [SerializeField] private GameObject Cards;
@@ -15,10 +17,35 @@ public class MainMenuManager : MonoBehaviour
    [Header("Initial Fade")] 
    [SerializeField] private CanvasGroup blackScreen;
    [SerializeField] private float duration;
-
-   private bool fadeIn;
+   
+   private bool InitialfadeOut;
    private bool fadeOut;
+   private bool fadeIn;
+   
+   [Header("Menu Panels")] 
+   [SerializeField] private GameObject MainMenuPanel;
+   [SerializeField] private GameObject OptionsPanel;
+   [SerializeField] private GameObject ChapterPanel;
+   [SerializeField] private GameObject CreditsPanel;
+   [SerializeField] private GameObject QuitPanel;
+   
+   [SerializeField] private float FadeDuration;
+   
+   [Header("First Buttons")] 
+   [SerializeField] private Button MainMenuFirstButton;
+   [SerializeField] private Button OptionsFirstButton;
+   [SerializeField] private Button ChapterFirstButton;
+   [SerializeField] private Button CreditsFirstButton;
+   [SerializeField] private Button QuitFirstButton;
+   
+   private CanvasGroup currentPanel;
+   
 
+
+   private void Awake()
+   {
+      Instance = this;
+   }
 
    private void Start()
    {
@@ -28,29 +55,99 @@ public class MainMenuManager : MonoBehaviour
 
    private void Update()
    {
-      if (fadeOut)
+      InitialFade();
+      
+      
+   }
+   
+   private void InitialFade()
+   {
+      if (!InitialfadeOut) return;
+      if (blackScreen.alpha >= 0)
       {
-         if (blackScreen.alpha >= 0)
-         {
-            blackScreen.alpha -= Time.deltaTime / duration;
-         }
-         if (blackScreen.alpha == 0)
-         {
-            fadeOut = false;
-            Cards.SetActive(true);
-            //TODO Lights Candles
-         }
+         blackScreen.alpha -= Time.deltaTime / duration;
       }
-      if (fadeIn)
+      if (blackScreen.alpha == 0)
       {
-         if (blackScreen.alpha < 1)
-         {
-            blackScreen.alpha += Time.deltaTime / duration;
-         }
-         if (blackScreen.alpha >= 1)
-         {
-            fadeIn = false;
-         }
+         fadeOut = false;
+         Cards.SetActive(true);
+         StartCoroutine(SelectPlayButton());
+         //TODO Lights Candles
       }
    }
+   
+   private void FadeOutPanels()
+   {
+      if (currentPanel.alpha >= 0)
+      {
+         currentPanel.alpha -= Time.deltaTime / FadeDuration;
+      }
+      if (currentPanel.alpha == 0)
+      {
+         fadeOut = false;
+      }
+   }
+   
+   private void FadeInPanels()
+   {
+      if (currentPanel.alpha <= 1)
+      {
+         currentPanel.alpha += Time.deltaTime / FadeDuration;
+      }
+      if (currentPanel.alpha >= 1)
+      {
+         fadeIn = false;
+      }
+   }
+   
+   
+   
+   private IEnumerator SelectPlayButton()
+   {
+      yield return new WaitForSeconds(2);
+      EventSystem.current.SetSelectedGameObject(PlayButton.gameObject);
+   }
+
+   public void OpenMenuPanel(float MenuId)
+   {
+      switch (MenuId)
+      {
+         case 0:
+            MainMenuPanel.SetActive(true);
+            OptionsPanel.SetActive(false);   
+            ChapterPanel.SetActive(false);
+            CreditsPanel.SetActive(false);
+            QuitPanel.SetActive(false);
+            break;
+         case 1:
+            OptionsPanel.SetActive(true);
+            MainMenuPanel.SetActive(false);
+            break;
+         case 2:
+            ChapterPanel.SetActive(true);
+            MainMenuPanel.SetActive(false);
+            break;
+         case 3:
+            MainMenuPanel.SetActive(false);
+            StartGame();
+            break;
+         case 4:
+            CreditsPanel.SetActive(true);
+            MainMenuPanel.SetActive(false);
+            break;
+         case 5:
+            QuitPanel.SetActive(true);
+            MainMenuPanel.SetActive(false);
+            break;
+            
+      }
+   }
+   
+   
+   private void StartGame()
+   {
+      //TODO Load Scene
+      Debug.Log("Start Game");
+   }
+   
 }
